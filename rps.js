96ab6d77ce997fccea999a0b802e6d0dc4d1210e
Jessica <jessicambromley7@gmail.com>
2022-05-21 13:37:12 -0400

    Add function shells

---
+++ rps.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+
+// function computerPlay() {
+
+// }
+
+// function playRound(playerSelection, computerSelection) {
+
+// }
+
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+
+//     }
+// }
