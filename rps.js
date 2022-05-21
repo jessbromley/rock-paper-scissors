256ab2415cb500eafb5bc783872769b9acaf3a5c
Jessica <jessicambromley7@gmail.com>
2022-05-21 13:57:30 -0400

    Add computerPlay function

--- rps.js
+++ rps.js
@@ -1,8 +1,10 @@
-console.log("hello world");
+//Gets a random item from choices (rock, paper, scissors) and returns it computerSelection
+function computerPlay() {
+    let options = Array("rock", "paper", "scissors");
+    return options[Math.floor(Math.random()*options.length)];
+}
 
-// function computerPlay() {
-
-// }
+computerSelection = computerPlay();
 
 // function playRound(playerSelection, computerSelection) {
 
